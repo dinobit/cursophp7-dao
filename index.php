be5c9856331fdbbb864acadc3362ea0dc601a0eb
Thiago Andrey chipset Ferreira <thiagoandrey.chip@gmail.com>
2018-08-08 11:43:22 -0300

    fizemos novos metodos de LISTA

--- index.php
+++ index.php
@@ -7,10 +7,22 @@
 //	echo json_encode($usuarios);
 
 
-	$root = new Usuario();
+//carrega 1 usuario
+	//$root = new Usuario();
+	//$root->loadbyId(4);
+ 	//echo $root;
 
-	$root->loadbyId(4);
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-	echo $root;
+//carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrga um usuário usando o login e a senha
+	$usuario = new Usuario();
+	$usuario->login("root","qwepoi");
+	echo $usuario;  //dar o echo vai invocar o metodo magico to_string que vai trazer um json
 
  ?>
